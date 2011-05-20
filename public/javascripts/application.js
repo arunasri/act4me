d697da0bf73417e3122efa13ac448a19e5d2dfee
Aruna Bolisetti <arunasri.42@gmail.com>
2011-05-20 17:31:09 -0400

    refactoring css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,57 +9,6 @@ $(document).ready(function() {
 		loadingText: "Loading more reviews...",
 		loadingImg: "/images/used/ajax-loading.gif",
 		donetext: "We've hit the end of the reviews."
-	},
-	function(elements) {
-		$("input[name='filter']:checked").triggerHandler('click');
-	});
-
-	$("input[name='filter']").click(function() {
-		$(".tweets ul li").hide();
-		if ($(this).val() == "all") {
-			$(".tweets ul li").show();
-		}
-		else {
-			$(".tweets ul li.js_" + $(this).val()).show();
-		}
-		return true;
-	});
-
-	$(".tweet").live("click", function() {
-		window.location = $(this).find(".avatar").attr("rel");
-		return true;
-	});
-
-	$("#jq-primarySearch").autocomplete({
-		source: "/movies/autocomplete",
-		minLength: 1,
-		select: function(event, ui) {
-			window.location = ui.item.url;
-		}
-	});
-
-	$("#destroy_all").live("click", function() {
-		if ($(this).is(":checked")) {
-			$("input[id$='_destroy']").attr('checked', 'checked')
-		} else {
-
-			$("input[id$='_destroy']").removeAttr('checked')
-		}
-	});
-
-	$("#feature_all").live("click", function() {
-		if ($(this).is(":checked")) {
-			$("input[id$='featured']").attr('checked', 'checked')
-		} else {
-
-			$("input[id$='featured']").removeAttr('checked')
-		}
-	});
-
-	$(".category_all").live('click', function() {
-		$("input[name$='[category]'][value='" + $(this).val() + "']").each(function() {
-			$(this).attr('checked', 'checked');
-		});
 	});
 });
 
