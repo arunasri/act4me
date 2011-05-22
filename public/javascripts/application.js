20bf68b203f67ed597ffdbeadbbaaef02caa4a14
Aruna Bolisetti <arunasri.42@gmail.com>
2011-05-22 13:31:06 -0400

    face book

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('ul#quotes').quote_rotator();
+	$('ul#quotes').quote_rotator();
 	// infinitescroll() is called on the element that surrounds
 	// the items you will be loading more of
 	$('#twitter-area .container_24').infinitescroll({
@@ -9,6 +9,12 @@ $(document).ready(function() {
 		loadingText: "Loading more reviews...",
 		loadingImg: "/images/used/ajax-loading.gif",
 		donetext: "We've hit the end of the reviews."
+	});
+	FB.init({
+		appId: '162180660488445',
+		status: true,
+		cookie: true,
+		xfbml: true
 	});
 });
 
