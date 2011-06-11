5fc550dd0fe552f033dccf3babe7be819f850c0d
Aruna <arunasri.42@gmail.com>
2011-06-11 12:26:11 -0400

    fixing mobile issues

--- vendor/plugins/mobile-fu/lib/mobile_fu.rb
+++ vendor/plugins/mobile-fu/lib/mobile_fu.rb
@@ -67,9 +67,8 @@ module ActionController
       # the user has opted to use either the 'Standard' view or 'Mobile' view.
 
       def set_mobile_format
-        Rails.logger.info "is mobile: #{is_mobile_device?}"
-        Rails.logger.info "is xhr: #{!request.xhr?}"
-        if is_mobile_device? && !request.xhr?
+        #if is_mobile_device? && !request.xhr?
+        if is_mobile_device?
           request.format = session[:mobile_view] == false ? :html : :mobile
           session[:mobile_view] = true if session[:mobile_view].nil?
         end
